f090b3dba0ae94370d1aed8f88fa74ca72182f2d
Johan Millers <johanoscarmarte@gmail.com>
2020-10-24 16:37:45 -0400

    se agredor el infiniteScroll  en el tabs 1

--- src/app/Services/news-services.service.ts
+++ src/app/Services/news-services.service.ts
@@ -8,12 +8,14 @@ import { Observable } from 'rxjs';
 })
 export class NewsServicesService {
   apiKey='e28b5afd2ce743eb854240fa9be5e090';
+  headlinepages = 0;
 
   constructor(private http: HttpClient) { }
 
 // perticion a la api para noticias TopHeadlines
   getTopHeadlines() {
-   return this.http.get<NewsReponses>(`https://newsapi.org/v2/top-headlines?country=mx&apiKey=${this.apiKey}`);
+    this.headlinepages ++;
+   return this.http.get<NewsReponses>(`https://newsapi.org/v2/top-headlines?country=mx&apiKey=${this.apiKey}&page=${this.headlinepages}`);
   }
   // perticion a la api para noticias TopHeadlines
 
--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { NewsReponses, Article } from './../../../Inteface/Interfaces';
 import { NewsServicesService } from './../../Services/news-services.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -10,14 +11,36 @@ import { Component, OnInit } from '@angular/core';
 export class Tab1Page implements OnInit {
   public Noticias: Article[] = [];
 
+  @ViewChild( IonInfiniteScroll ) inifiteScroll: IonInfiniteScroll;
+
   constructor(private NewsServicesService: NewsServicesService) {}
 
   ngOnInit() {
-    this.NewsServicesService.getTopHeadlines().subscribe(news => {
-      console.log(news);
-      this.Noticias.push(...news.articles)
+    this.cargarNoticias();
+  }
 
-    })
+  cargarNoticias(event?){
+    this.NewsServicesService.getTopHeadlines()
+    .subscribe(news => {
+      console.log(news);
+      //condicion para evaluar si hay mas articulos que mostral
+      if (news.articles.length === 0){
+        this.inifiteScroll.disabled = true;
+        this.inifiteScroll.complete();
+        return;
+      }
+       //condicion para evaluar si hay mas articulos que mostral
+      this.Noticias.push(...news.articles);
+
+      if (event) {
+        this.inifiteScroll.complete();
+      }
+    });
+  }
+
+  loadData(event){
+    console.log(event);
+    this.cargarNoticias(event);
   }
 
   
