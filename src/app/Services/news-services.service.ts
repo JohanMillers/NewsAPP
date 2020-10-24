dd927855c19ec93a713e857cb97c6e330bb8b220
Johan Millers <johanoscarmarte@gmail.com>
2020-10-24 11:32:01 -0400

    se la cargar de noticias por categoria

--- src/app/Services/news-services.service.ts
+++ src/app/Services/news-services.service.ts
@@ -7,14 +7,17 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class NewsServicesService {
+  apiKey='e28b5afd2ce743eb854240fa9be5e090';
 
   constructor(private http: HttpClient) { }
 
 // perticion a la api para noticias TopHeadlines
   getTopHeadlines() {
-   return this.http.get<NewsReponses>(`https://newsapi.org/v2/top-headlines?country=us&apiKey=e28b5afd2ce743eb854240fa9be5e090`);
+   return this.http.get<NewsReponses>(`https://newsapi.org/v2/top-headlines?country=mx&apiKey=${this.apiKey}`);
   }
   // perticion a la api para noticias TopHeadlines
 
-
+  getNewsforCategory(categoria:string){
+    return this.http.get<NewsReponses>(`https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=${this.apiKey}`);
+  }
 }
--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Article } from './../../../Inteface/Interfaces';
+import { NewsServicesService } from './../../Services/news-services.service';
+import { IonSegment } from '@ionic/angular';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  @ViewChild( IonSegment ) Segment:IonSegment;
 
-  constructor() {}
+  categorias = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+  category:any = [];
+  Noticias: Article[] = [];
+
+  constructor(private NewsServices: NewsServicesService) {}
+
+  
+  ngOnInit(): void {
+    this.category = this.categorias[2];
+
+    this.cargarNoticias(this.categorias[2])
+    
+  }
+  //Funcion que esta pentiente del cambio de Segment
+  segmentChanged(ev: any) {
+    this.Noticias = [];
+    // console.log(ev.detail.value);
+    this.cargarNoticias(ev.detail.value);
+  }
+  //Funcion que esta pentiente del cambio de Segment
+  
+  //Funcion que cargar las noticias
+  cargarNoticias(categoria: string){
+    this.NewsServices.getNewsforCategory(categoria).subscribe(resp =>{
+      console.log(resp);
+      console.log(categoria)
+      this.Noticias.push(...resp.articles);
+    })
+  }
+  //Funcion que cargar las noticias
+
+  
+
+ 
 
 }
